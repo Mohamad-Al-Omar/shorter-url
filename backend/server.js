3fbe4662dc1264c7d1e937b7860a47d6ce79bdb6
Mohamad Al Omar <m.al.omar94@gmail.com>
2023-05-17 01:52:45 +0400

    Add deta as database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const { Deta } = require("deta");
+
 const app = express();
-const port = 3001;
+const port = process.env.PORT;
 
 // Middleware to enable CORS
 app.use(cors());
@@ -9,27 +11,66 @@ app.use(cors());
 // Middleware to parse JSON data in the request body
 app.use(express.json());
 
+// Regular expression to validate URLs
+const urlRegex =
+  /^(?:https?:\/\/)?(?:www\.)?[\w.-]+\.[a-z]{2,}(?:\/[\w.-]*)*(?:\?[\w.%&+=-]+)?(?:#[\w-]*)?$/i;
+
+// Initialize with a Project Key
+// locally, set the project key in an env var called DETA_PROJECT_KEY
+const deta = Deta(process.env.PROJECT_KEY);
+
+// This how to connect to or create a database.
+const linksDB = deta.Base("links");
+
 // GET service
-app.get("/api/:inputString", (req, res) => {
+app.get("/json/:inputString", async (req, res) => {
   const inputString = req.params.inputString;
-  const redirectUrl = `https://www.example.com/?input=${inputString}`;
-  res.redirect(redirectUrl);
+  const { items } = await linksDB.fetch({ shortId: inputString });
+  const rootUrl = `${req.protocol}://${req.get("host")}`;
+  const shortUrl = `${rootUrl}/api/${inputString}`;
+  const successResponse = {
+    data: {
+      url: items[0].url,
+      short_url: shortUrl,
+    },
+    success: true,
+  };
+  res.json(successResponse);
 });
 
 // POST service
-app.post("/api", (req, res) => {
-  const inputData = req.body;
-  const outputData = {
-    message: `You sent: ${inputData.url}`,
-    status: "success",
+app.post("/", async (req, res) => {
+  const { url } = req.body;
+
+  if (!urlRegex.test(url)) {
+    res.status(400).json({ message: "Invalid URL", success: false });
+    return;
+  }
+  const { nanoid } = await import("nanoid");
+  const shortId = nanoid(8);
+  const rootUrl = `${req.protocol}://${req.get("host")}`;
+  const shortUrl = `${rootUrl}/api/${shortId}`;
+  await linksDB.put({
+    shortId: shortId,
+    url: url,
+    key: shortId,
+  });
+  const successResponse = {
+    data: {
+      new_url: shortUrl,
+      short_id: shortId,
+    },
+    success: true,
   };
-  res.json(outputData);
+  res.json(successResponse);
 });
 
 // Redirect service
-app.get("/:shortId", (req, res) => {
+app.get("/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
-  const inputUrl = urlDatabase[shortId];
+  const { items } = await linksDB.fetch({ shortId: shortId });
+  const inputUrl = items[0]?.url;
+  console.log("inputUrl => ", inputUrl);
   if (inputUrl) {
     res.redirect(inputUrl);
   } else {
