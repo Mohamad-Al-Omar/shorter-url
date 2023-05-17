2849beea55a3bd713f3ec7cbd6d9b041d63e92fd
Mohamad Al Omar <m.al.omar94@gmail.com>
2023-05-17 23:36:22 +0400

    Update

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Full config:  https://github.com/axios/axios#request-config
 axios.defaults.baseURL =
-  process.env.baseURL || process.env.apiUrl || import.meta.env.VITE_API_URL;
+  process.env.VITE_API_URL || import.meta.env.VITE_API_URL || "/api/";
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
